2dad2d3b15dfdac86a30a367d49cc75d502b6366
Aviral1611 <90606176+Aviral1611@users.noreply.github.com>
2023-07-13 16:36:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+
+
+const button = document.getElementById('mack');
+console.log(true);
+
+button.addEventListener('click', function() {
+  
+  alert('Your Details have been sent!');
+});
+
+
+
+
